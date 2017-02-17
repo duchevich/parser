b3aed975cbe1bb773419926200424614d1066004
duchevich <duchevich274@gmail.com>
2017-02-17 13:59:04 +0200

    cor1

--- index.php
+++ index.php
@@ -29,26 +29,26 @@
 		<div class="container">
 			<div id="news-block" class="news">
 				<?php 
+			        #массив заголовков новостей
+			        $head = [
+			            ['title' => 'Головне', 'subtitle' => 'всі головні події дня'],
+			            ['title' => 'Політика', 'subtitle' => 'всі політичні новини України'],
+			            ['title' => 'Економіка', 'subtitle' => 'всі новини економіки та фінансів'],
+			            ['title' => 'Події', 'subtitle' => 'всі новини подій, аварій, катастроф'],
+			            ['title' => 'Суспільство', 'subtitle' => 'всі новини розділу Суспільство'],
+			            ['title' => 'Новини регіонів України', 'subtitle' => 'всі новини розділу'],
+			            ['title' => 'Технології', 'subtitle' => 'всі новини інформаційних технологій'],
+			            ['title' => 'Наука', 'subtitle' => 'всі новини науки'],
+			            ['title' => 'Авто', 'subtitle' => 'всі автоновини'],
+			            ['title' => 'Спорт', 'subtitle' => 'всі спортивні новини: футбол, бокс, хокей...'],
+			            ['title' => 'Здоров\'я', 'subtitle' => 'всі новини медицини, краси та косметології'],
+			            ['title' => 'Шоу-бізнес', 'subtitle' => 'всі новини розділу Шоу-бізнес'],
+			            ['title' => 'За кордоном', 'subtitle' => 'всі новини з-за кордону'],
+			            ['title' => 'Курйози', 'subtitle' => 'всі забавні, курйозні новини'],
+			            ['title' => 'Фоторепортаж', 'subtitle' => 'всі фотоновини'],
+			            ['title' => 'Відео', 'subtitle' => 'всі відео новини']
+			        ];
 
-        #
-        $head = [
-            ['title' => 'Головне', 'subtitle' => 'всі головні події дня'],
-            ['title' => 'Політика', 'subtitle' => 'всі політичні новини України'],
-            ['title' => 'Економіка', 'subtitle' => 'всі новини економіки та фінансів'],
-            ['title' => 'Події', 'subtitle' => 'всі новини подій, аварій, катастроф'],
-            ['title' => 'Суспільство', 'subtitle' => 'всі новини розділу Суспільство'],
-            ['title' => 'Новини регіонів України', 'subtitle' => 'всі новини розділу'],
-            ['title' => 'Технології', 'subtitle' => 'всі новини інформаційних технологій'],
-            ['title' => 'Наука', 'subtitle' => 'всі новини науки'],
-            ['title' => 'Авто', 'subtitle' => 'всі автоновини'],
-            ['title' => 'Спорт', 'subtitle' => 'всі спортивні новини: футбол, бокс, хокей...'],
-            ['title' => 'Здоров\'я', 'subtitle' => 'всі новини медицини, краси та косметології'],
-            ['title' => 'Шоу-бізнес', 'subtitle' => 'всі новини розділу Шоу-бізнес'],
-            ['title' => 'За кордоном', 'subtitle' => 'всі новини з-за кордону'],
-            ['title' => 'Курйози', 'subtitle' => 'всі забавні, курйозні новини'],
-            ['title' => 'Фоторепортаж', 'subtitle' => 'всі фотоновини'],
-            ['title' => 'Відео', 'subtitle' => 'всі відео новини']
-        ];
 					#соединение с базой данных
 					$host = 'localhost';
 					$db = 'newsSite';
@@ -62,46 +62,33 @@
 					);
 					$db = new PDO($dsn, $user, $pass, $opt);
 
-					#
+					#вывод новостей из базы данных
 					$query = $db->query("SELECT max(data) data FROM news");
 					$row = $query->fetch();
 					$data = $row['data'];
 					echo '<p class="data">' . $data . '</p>';
-					/*echo '<pre>';
-			        print_r($head); 
-			        echo '</pre>';*/
-
-			        for ($i=0; $i < count($head); $i++) { 
-						#echo $head[$i]['title'] . '<br>';
+					for ($i=0; $i < count($head); $i++) {
 						echo '<h3>' . $head[$i]['title'] . '</h3>';
              			echo '<h4>' . $head[$i]['subtitle'] . '</h4>';
-
-						$query = $db->prepare("SELECT title, url, data FROM news WHERE data = :data AND htitle = :htitle");
+						$query = $db->prepare("SELECT title, url FROM news WHERE data = :data AND htitle = :htitle");
 	        			$query->execute(array('data' => $data, 'htitle' => $head[$i]['title']));
 			            while ($row = $query->fetch())
 						{
 							$url = $row['url'];
-							echo "<p><a href='$url'>" . $row['title'] . $row['data'] . '</a></p>';
-							
+							echo "<p><a href='$url'>" . $row['title'] . '</a></p>';
 						}
-
+						echo '<hr>';
 					}
-
-        			
-
-
-
 				 ?>
 			</div>
-			
 		</div>
 	</section>
 	<footer>
-		
+		<h3>2017</h3>		
 	</footer>
 	<script type="text/javascript">
 		function fync() {
-		$("#news-block").empty().load("par.php", function() {
+		$("#news-block").load("par.php", function() {
 		});
 		}; 
 	</script>
--- par.php
+++ par.php
@@ -6,6 +6,7 @@
 </head>
 <body>
     <?php
+        error_reporting('E_ALL');
 
         #соединение с базой данных
         $host = 'localhost';
@@ -20,10 +21,10 @@
         );
         $db = new PDO($dsn, $user, $pass, $opt);
 
-        #
+        #заголовки
         $url = "https://www.ukr.net/";
         $headers = get_headers($url, 1);
-        file_put_contents('headers.log',var_export($headers,true));
+        file_put_contents('headers.log',var_export($headers,true), FILE_APPEND);
         
         #парсинг
         $url = file_get_contents("https://www.ukr.net/ajax/news.json");
@@ -31,10 +32,8 @@
         $data = date("Y-m-d H:i:s");
         echo '<p class="data">' . $data . '</p>';
         $sectionCount = count($json['news']);
-        #echo $sectionCount . '<br>';
         for ($i=0; $i < $sectionCount; $i++) {
              $count = count($json['news'][$i]['items']);
-             #echo $count . '<br>';
              echo '<h3>' . $json['news'][$i]['title'] . '</h3>';
              echo '<h4>' . $json['news'][$i]['readMore'] . '</h4>';
              $htitle = $json['news'][$i]['title'];
@@ -48,10 +47,6 @@
             }
             echo '<hr>';
         }
-        /*echo '<pre>';
-        print_r($json['news']); 
-        echo '</pre>';*/
-
 ?>
 </body>
 </html>
